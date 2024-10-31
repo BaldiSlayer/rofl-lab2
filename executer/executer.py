ecb45914e9ff7008e919fb7b3a5e9a71f48335cc
HumsterProgrammer <denis_ka_2307@mail.ru>
2024-10-31 20:02:26 +0300

    test executer

--- executer/executer.py
+++ executer/executer.py
@@ -3,23 +3,32 @@
 import sys
 import subprocess as sp
 
+
+
 def runSolve(MATpath, solverpath):
-    MAT = sp.Popen(MATpath, stdout=sp.PIPE, stdin=sp.PIPE)
-    solver = sp.Popen(solverpath, stdout=sp.PIPE, stdin=sp.PIPE)
-    solvermessage, _ = solver.communicate()    
-    while solver.poll() == None:
-        print(solvermessage.decode())
-        MATmessage, _ = MAT.communicate(solvermessage)
-        print(MATmessage.decode())
-        solvermessage, _ = solver.communicate(MATmessage)
+    MAT = sp.Popen(MATpath, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE,text=True, bufsize=1)
+    solver = sp.Popen(solverpath, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE, text=True, bufsize=1)
+    try:
+        solver.stdin.write("isin\nS\n")
+        solver.stdin.flush()
+        s = solver.stdout.readline().strip()
+        print(s)
+    except:
+        for line in solver.stderr:
+            print("Solver-err: "+line, end="")
+        for line in MAT.stderr:
+            print("MAT-err: "+line, end="")
+    finally:
+        MAT.kill()
+        solver.kill()
         
 
 if __name__ == "__main__":
     MAT = "./MAT"
     solver = "./Solver"    
     if len(sys.argv) == 3: #executer.pu + MAT path + solver path
-        MAT, solver = sys.argv[1:]
+        MAT, solver = list(map(lambda x: x.split(" "), sys.argv[1:]))
     
-    print("used mat: {0}\nsolver: {1}\n".format(MAT, solver))
+    print("used mat: {0}\nsolver: {1}".format(MAT, solver))
     #optional: check if it MAT and if it solver    
     runSolve(MAT, solver)
