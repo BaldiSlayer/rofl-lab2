9d4f6fa689b0bbfcb841e4a5713d04beb1a05c2b
HumsterProgrammer <denis_ka_2307@mail.ru>
2024-11-01 20:47:52 +0300

    Доработано взаимодействие Для дальнейшей отладки нужны рабочие МАТ и солвер

--- executer/executer.py
+++ executer/executer.py
@@ -7,18 +7,17 @@ import subprocess as sp
 
 def runSolve(MATpath, solverpath):
     MAT = sp.Popen(MATpath, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE,text=True, bufsize=1)
-    solver = sp.Popen(solverpath, stdout=sp.PIPE, stdin=sp.PIPE, stderr=sp.PIPE, text=True, bufsize=1)
+    solver = sp.Popen(solverpath, stdout=MAT.stdin, stdin=MAT.stdout, stderr=sp.PIPE, text=True, bufsize=1)
     try:
-        solver.stdin.write("isin\nS\n")
-        solver.stdin.flush()
-        s = solver.stdout.readline().strip()
-        print(s)
+        MAT.wait()
     except:
         for line in solver.stderr:
             print("Solver-err: "+line, end="")
         for line in MAT.stderr:
             print("MAT-err: "+line, end="")
     finally:
+        print("MAT-err:" + MAT.stderr.read())
+        print("solver-err:"+solver.stderr.read())
         MAT.kill()
         solver.kill()
         
